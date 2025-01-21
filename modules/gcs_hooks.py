d9e6e2c92503064bc080476abf537331ea59e3e3
OfirDataloopAI <ofir.g@dataloop.ai>
2025-01-21 10:06:03 +0200

    updated exception message

--- modules/gcs_hooks.py
+++ modules/gcs_hooks.py
@@ -25,9 +25,9 @@ class GCSExport(dl.BaseServiceRunner):
             credentials_json = json.loads(decoded_credentials)
             credentials = json.loads(credentials_json['content'])
         except Exception:
-            raise ValueError(f"Failed to decode the service account json. "
-                             f"Follow this link ReadMe to check how to properly use GCP service account with Dataloop:"
-                             f"https://github.com/dataloop-ai-apps/export-gcs/blob/main/README.md")
+            raise ValueError(f"Unable to decode the service account JSON. "
+                             f"Please refer to the following guide for proper usage of GCP service accounts with"
+                             f"Dataloop: https://github.com/dataloop-ai-apps/export-gcs/blob/main/README.md")
         self.client = storage.Client.from_service_account_info(info=credentials)
 
     def export_annotation(self, item: dl.Item, context: dl.Context):
